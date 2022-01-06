2c258885ba848ff20ab9685d0e10e0c8ac116420
Michele Viganò <74900205+Mike02vigaiga@users.noreply.github.com>
2022-01-06 15:19:07 +0100

    Update words_counter.sh

--- words_counter.sh
+++ words_counter.sh
@@ -1,27 +1,27 @@
-
 #!/bin/bash
 
 #exit the sript if an error occurs-----
 set -e
 
 #help menu-------------
-if[$# = 0];
-then
-echo"Try $0 --help"
-else
-if[$1 = "--help"];
-then
-echo="Usage: ./$0 [/location/FILENAME] [WORD] "
-echo="the scrip will search how many times 'WORD' appear in 'FILENAME' "
-echo="--"
-echo="Full documentation at: <https://github.com/Mike02vigaiga/scripting-linux-4school/>"
-else
+if [[ $# -eq 0 || $# -eq 1 ]];then
+echo "Try:'$0 --help'"
+if [ $1 == "--help" ];then
+ak=2
+echo "Usage: $0 [/location/FILENAME] [WORD] "
+echo "the scrip will search how many times 'WORD' appear in 'FILENAME' "
+echo "--"
+echo "Full documentation at: <https://github.com/Mike02vigaiga/scripting-linux-4school/>"
 fi
-fi
+else
 #script----------------
-if[-e $1 && -r $1];
-then
-count=$(grep -c '$2' $1)
-echo="the string '$2' occours $count times in file '$1' "
-
+if [[ -e $1 ]];then
+count=$(grep -c $2 $1)
+al=2
+echo "the string '$2' occours $count times in file '$1' "
+elif [[ $ak != 2 ]];then #se la variabile di controllo è =2 significa che la var $1 esiste ma come --help e non come file
+echo "the file $1 does not exist"
+elif [[ $al -eq 2 ]];then
+echo "error"
+fi
 fi
